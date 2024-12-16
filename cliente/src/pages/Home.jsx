c044d2d90b3687ae5abaf71337da518794089a58
weslley1118 <nakamurahenrique31@gmail.com>
2024-12-16 13:25:58 -0400

    é nois

--- cliente/src/pages/Home.jsx
+++ cliente/src/pages/Home.jsx
@@ -15,8 +15,17 @@ export default function Home() {
       }
     }
     buscarUsuario();
-  }, [])
+  }, [usuarios]);
 
+  const deletar = async=>{
+    try{
+      await fetch ('http://localhost:3000/usuarios'+ id,{
+        method:'DELETE'
+      });
+    }catch{
+      alert("");
+    }
+}
   return (
     <table>
       <tr>
@@ -27,8 +36,12 @@ export default function Home() {
         <tr key={usuario.id}>
           <td>{usuario.nome}</td>
           <td>{usuario.email}</td>
+          <td><button onAuxClick={()=> deletar(usuario.id)}>X</button></td>
         </tr>
       )}
+     
     </table>
   );
+
+
 }
--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,9 +1,56 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 export default function Registrar() {
+  const [nome,setNome] = useState('');
+  const [email,setEmail] = useState('');
+
+  const navigate = useNavigate();
+
+  const registrer =async(event) =>{
+    event.preventDafault();
+    try {
+    const resposta = await fetch("http://localhost:3000",{
+      method: 'POST',
+      headers:{'content-type':'application/json'},
+      boody: JSON.stringify({
+        nome:nome,
+        email:email
+    })
+  });
+ 
+ if(reposta.ok){
+  navigate('/');
+ }
+}
+catch (err){
+  alert( 'Erro no registro!',err);
+}
 
 
-  return (
+}
+  return (<main>
+    <forn action=""onSubmit={Registrar}>
+      <input
+      type="text"
+      value={nome}
+      onChange={(event)=>setNome(event.target.value)}
+      />
+      <input
+      type="email"
+      value={email}
+      onChange={(event)=>setEmail(event.target.value)}
+      />
+      </forn>
+      <button onClick={Registrar}>salvar</button>
+      <a href="http://localhost:3000/usuarios" className={énois}>
+        Verificar Banco de Dados
+      </a>
+
+
+    
+  </main>
         
   );
 }
